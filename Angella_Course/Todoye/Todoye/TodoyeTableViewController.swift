89dca37d7f57f5f586e2db082efa85e44eb3f88c
Pavel71 <raz_3@mail.ru>
2019-02-22 16:26:49 +0300

    Todoey

---
+++ Angella_Course/Todoye/Todoye/TodoyeTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  Todoye
+//
+//  Created by PavelM on 22/02/2019.
+//  Copyright © 2019 PavelM. All rights reserved.
+//
+
+import UIKit
+
+class TodoyeTableViewController: UITableViewController {
+    
+    
+    var itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // так как TableViewController  Delegate Datasorce не надо прописывать
+        
+    }
+    
+    
+    //MARK: - TableViewDataSource Methods
+    
+    // Сколько строчек в секторе
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+    }
+    
+    // Какой тип ячейки отобразить
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    // Срабатывает когда мы выбираем строку
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(itemArray[indexPath.row])
+        
+        // Поставить марку что выбрали
+        
+        let markEnum = tableView.cellForRow(at: indexPath)?.accessoryType
+        
+        tableView.cellForRow(at: indexPath)?.accessoryType = markEnum == .checkmark ? .none : .checkmark
+
+        
+        // Снять выбор со строчки
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        // при нажате на + Добавим новые данные в таблицу! Используем Алерт Контроллер
+        
+        let controller = UIAlertController(title: "Добавим в таблицу", message: "Напишите задание", preferredStyle: .alert)
+        
+        // 2 По нажатию кнопку запускается это замыкание
+        let action = UIAlertAction(title: "Добавить", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+
+        }
+        
+        // 1 Создается поле в контроллере
+        controller.addTextField { (controllerTextField) in
+            controllerTextField.placeholder = "Пишите сюда"
+            
+            // присваиваем локальной переменной ссылк на объект
+            textField = controllerTextField
+            
+        }
+        
+       
+        
+        controller.addAction(action)
+        
+        present(controller,animated: true,completion: nil)
+    }
+    
+}
+
